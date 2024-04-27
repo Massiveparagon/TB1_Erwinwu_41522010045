5912787eabb1dd64efb925332a37762d5ff66abd
Massiveparagon <erwinwu123@gmail.com~>
2024-04-27 21:13:45 +0700

    first commit

---
+++ app/src/main/java/com/example/tb1/navigation_drawer.java
@@ -0,0 +1,43 @@
+package com.example.tb1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class navigation_drawer extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageButton buttonDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_navigation_drawer);
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+        buttonDrawerToggle=findViewById(R.id.buttonDrawerToggle);
+
+        buttonDrawerToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
